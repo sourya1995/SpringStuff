0dcbfed1e802a45e7c9ce861940866ba17248eb4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-13 17:13:33 +0530

    using query parameters

--- MVC/src/main/java/com/sourya/MVC/controllers/MainController.java
+++ MVC/src/main/java/com/sourya/MVC/controllers/MainController.java
@@ -2,12 +2,13 @@ package com.sourya.MVC.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class MainController {
 	@RequestMapping("/home")
-	public String home(Model page) {
-		page.addAttribute("username", "Katy");
-		page.addAttribute("color", "red");
+	public String home(@RequestParam(required=false) String color, @RequestParam(required=false) String name, Model page) {
+		page.addAttribute("username", name);
+		page.addAttribute("color", color);
 		return "home.html";
 	}
 }
