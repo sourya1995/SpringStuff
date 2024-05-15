3d33660b95d71e9b949ab3bb8ff5cf8670c23f62
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-15 18:38:42 +0530

    Application Scope

--- MVC-1/src/main/java/com/sourya/WebScope/LoginProcessor.java
+++ MVC-1/src/main/java/com/sourya/WebScope/LoginProcessor.java
@@ -10,20 +10,26 @@ public class LoginProcessor {
 	private String password;
 	
 	private final LoggedUserManagementService loggedUserManagementService;
+	private final LoginCountService loginCountService;
 	
-	public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+	public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
 		this.loggedUserManagementService = loggedUserManagementService;
+		this.loginCountService = loginCountService;
 	}
 
 	public boolean login() {
+		loginCountService.increment();
 		String username = this.getUsername();
 		String password = this.getPassword();
 		
+		boolean loginResult = false;
+		
 		if("natalie".equals(username) && "password".equals(password)) {
-			return true;
-		} else {
-			return false;
-		}
+			loginResult = true;
+			loggedUserManagementService.setUsername(username);
+		} 
+		
+		return loginResult;
 	}
 
 	public String getUsername() {
--- MVC-1/src/main/java/com/sourya/WebScope/MainController.java
+++ MVC-1/src/main/java/com/sourya/WebScope/MainController.java
@@ -1,11 +1,14 @@
 package com.sourya.WebScope;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class MainController {
 	private final LoggedUserManagementService loggedUserManagementService;
+	private final LoginCountService loginCountService;
 	
 	public MainController(LoggedUserManagementService loggedUserManagementService) {
 		this.loggedUserManagementService = loggedUserManagementService;
@@ -18,10 +21,14 @@ public class MainController {
 			loggedUserManagementService.setUsername(null);
 		}
 		String username = loggedUserManagementService.getUsername();
+		int count = loginCountService.getCount();
 		
 		if(username == null) {
 			return "redirect:/";
 		}
+		
+		model.addAttribute("username", username);
+		model.addAttribute("loginCount", count);
 		
 		return "main.html";
 	}
