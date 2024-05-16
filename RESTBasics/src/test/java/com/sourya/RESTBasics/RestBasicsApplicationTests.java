8beec0b2e9100e1080756737c9de46d664c742b7
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-16 07:19:08 +0530

    Basic REST controller

---
+++ RESTBasics/src/test/java/com/sourya/RESTBasics/RestBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourya.RESTBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
