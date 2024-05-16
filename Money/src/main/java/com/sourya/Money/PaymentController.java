854a7bfcf69ae522df299e50317dd48781e990d6
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-16 10:46:13 +0530

    Using request body to get data from client

--- Money/src/main/java/com/sourya/Money/PaymentController.java
+++ Money/src/main/java/com/sourya/Money/PaymentController.java
@@ -1,12 +1,16 @@
 package com.sourya.Money;
 
+import java.util.logging.Logger;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
+	private static Logger LOGGER = Logger.getLogger(PaymentController.class.getName());
 	private final PaymentService paymentService;
 
 	public PaymentController(PaymentService paymentService) {
@@ -14,9 +18,11 @@ public class PaymentController {
 	}
 	
 	@PostMapping("/payment")
-	public ResponseEntity<PaymentDetails> makePayment() {
+	public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
 		
-			PaymentDetails paymentDetails = paymentService.processPayment();
+			/*PaymentDetails paymentDetails = paymentService.processPayment();*/
+		
+		LOGGER.info("received payment" + paymentDetails.getAmount());
 			
 			return ResponseEntity
 					.status(HttpStatus.ACCEPTED)
@@ -25,4 +31,4 @@ public class PaymentController {
 	}
 	
 	
-}
+
