82137b389201f4ca6ef69b7840d0b7ab99e04bc9
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2024-05-19 16:57:04 +0530

    custom Data Source configuration

---
+++ SpringDatabase-1/src/main/java/com/example/SpringDatabase1/SpringDatabase1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringDatabase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabase1Application.class, args);
+	}
+
+}
---
+++ SpringDatabase-1/src/test/java/com/example/SpringDatabase1/SpringDatabase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDatabase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
